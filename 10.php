f38ff2910ac53825565c15164e83f2cb543f127f
Oleksii Polubotko <for.alexeyp@gmail.com>
2019-02-27 14:58:33 +0200

    task 10

---
+++ 10.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>10</title>
+</head>
+<body>
+
+<p>10. Выведите столбец чисел от 11 до 33.</p>
+
+</body>
+</html>
+
+<?php
+
+for ($i = 11; $i <= 33; $i++) {
+    echo "$i <br>";
+}
+
+echo '<hr>';
+
+$m = 11;
+while ($m <= 33) {
+    echo $m;
+    echo "<br>";
+    $m++;
+}
