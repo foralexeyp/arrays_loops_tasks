e78b1af7218b5a6d605e75ee66108355e08c1af8
Oleksii Polubotko <for.alexeyp@gmail.com>
2019-02-27 14:37:52 +0200

    task 8

---
+++ 8.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>8</title>
+</head>
+<body>
+
+<p>8. Дан массив с элементами 1, 2, 3, 4, 5, 6, 7, 8, 9.<br>
+    С помощью цикла foreach создайте строку '123456789'.</p>
+
+</body>
+</html>
+
+<?php
+
+$arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+foreach ($arr as $item) {
+    echo $item;
+}
