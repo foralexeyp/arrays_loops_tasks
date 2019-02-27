517f7660525791d223d302148a6336d3bc505a2f
Oleksii Polubotko <for.alexeyp@gmail.com>
2019-02-27 14:11:38 +0200

    task 6

---
+++ 6.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>6</title>
+</head>
+<body>
+
+<pre>6. Дан массив $arr. С помощью цикла foreach запишите английские названия в массив $en, а
+    русские — в массив $ru.
+    $arr = array('green'=>'зеленый', 'red'=>'красный','blue'=>'голубой');
+    $en = array('green', 'red','blue');
+    $ru = array('зеленый', 'красный', 'голубой');</pre>
+
+</body>
+</html>
+
+<?php
+$arr = array('green'=>'зеленый', 'red'=>'красный','blue'=>'голубой');
+$en = [];
+$ru = [];
+
+foreach ($arr as $key => $k) {
+    $en[] = $key;
+    $ru[] = $k;
+}
+echo '<pre>';
+print_r($en);
+echo '</pre>';
+
+echo '<pre>';
+print_r($ru);
+echo '</pre>';
