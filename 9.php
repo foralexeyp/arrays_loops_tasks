c827a877956731f33bc2991567f47fc4b67db808
Oleksii Polubotko <for.alexeyp@gmail.com>
2019-02-27 14:45:35 +0200

    task 9

---
+++ 9.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>9</title>
+</head>
+<body>
+
+<p>9. Выведите столбец чисел от 1 до 100.</p>
+
+</body>
+</html>
+
+<?php
+
+for ($i = 1; $i <= 100; $i++)  {
+    echo "$i <br>";
+}
