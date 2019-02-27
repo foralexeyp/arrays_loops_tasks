d2b07914b928f0aa2945d1bdfbbb81ad97a24d86
Oleksii Polubotko <for.alexeyp@gmail.com>
2019-02-27 15:02:56 +0200

    atsk 11

---
+++ 11.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>11</title>
+</head>
+<body>
+
+<p>11. Выведите столбец четных чисел в промежутке от нуля до 100.</p>
+
+</body>
+</html>
+
+<?php
+
+for ($i = 0; $i <= 100; $i++) {
+    if ($i % 2 == 0) {
+        echo $i;
+        echo '<br>';
+    }
+}
